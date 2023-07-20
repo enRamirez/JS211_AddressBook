dd7864b22dcdb45d203a135cec73b17b854198ac
enRamirez <ricky.ramirez125042@gmail.com>
2023-07-20 16:21:32 -0500

    Finishing up on main.js

---
+++ main.js
@@ -0,0 +1,56 @@
+// fetch a user and display them one at a time.
+window.onload = function() {
+    addressBook()
+}
+
+let people = [];
+const container = document.getElementById('container')
+
+const API = 'https://randomuser.me/api/?results=5'
+
+const addressBook = () => {
+  fetch(API)
+    .then(res => {
+        if(!res.ok) {
+            throw Error(res.statusText)
+        }return res.json()
+    })
+    .then(data => {
+        console.log(data)
+        people = data.results
+    })
+    .catch(err => console.log(`Error, ${err}`))
+    .then(() => {
+        people.forEach((person) => {
+            let content = document.createElement("div")
+            content.innerHTML = `
+            <h4>${person.name.first} ${person.name.last}</h4>
+            <img src="${person.picture.large}"/>
+
+            <button id="${person.login.uuid}" onclick="showHide(this)">More Info/Less Info</button>
+
+            <div class="moreInfo" id="${person.login.uuid}-person">
+            Email: ${person.email} <br>
+            Phone: ${person.phone} <br>
+            Address: ${person.location.street.number} ${person.location.street.name}, 
+            ${person.location.city}, ${person.location.state} ${person.location.postcode}
+            </div>
+            `
+
+            container.appendChild(content)
+        })
+    })    
+}
+
+
+const showHide = (button) => {
+    console.log(button)
+    let x = document.getElementById(button.id+"-person")
+    console.log(x,'this is the el clicked')
+    console.log(x.style.display,'this is the current style')
+    if(x.style.display === "none" || !x.style.display) {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
+}
